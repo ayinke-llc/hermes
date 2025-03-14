9fe8f67cd262e18cb5a7cd0c49eff446d3a73522
Lanre Adelowo <yo@lanre.wtf>
2025-03-14 23:57:20 +0100

    generate random string

--- strings.go
+++ strings.go
@@ -1,5 +1,19 @@
 package hermes
 
-import "strings"
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"strings"
+)
 
 func IsStringEmpty(s string) bool { return len(strings.TrimSpace(s)) == 0 }
+
+func Random(length int) (string, error) {
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.RawURLEncoding.EncodeToString(bytes), nil
+}
